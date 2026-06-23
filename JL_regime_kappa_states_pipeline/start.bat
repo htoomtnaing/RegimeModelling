b71503d5a9e3a4b90af7af390d7d29c13d33d066
Tan Wei Lin, Gregory <gregtanwl@gmail.com>
2026-06-24 03:31:01 +0800

    Delete JL_regime_kappa_states_pipeline directory

--- JL_regime_kappa_states_pipeline/start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM -- Launch Jupyter Lab to view regime_kappa_states.ipynb --
-cd /d "%~dp0"
-if not exist ".venv\Scripts\activate.bat" (
-  echo No virtual environment found. Run setup.bat first.
-  pause & exit /b 1
-)
-call .venv\Scripts\activate.bat
-jupyter lab regime_kappa_states.ipynb
